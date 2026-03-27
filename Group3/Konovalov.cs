3440ec7c4f6c7446d21059bdb464c1fdc34a8ed2
Konstantin <H1dra5@yandex.ru>
2026-03-27 09:23:38 +0700

    Сделал полную реализацию своего героя, чуточку написал историю

--- Group3/Konovalov.cs
+++ Group3/Konovalov.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ForkKnights.Group3 {
   public class Konovalov : IKnight {
-    string name, power, fromUniverse, species, status, isHeAHero;
+    string name, power, fromUniverse, species, status;
     int age;
-  }
+    bool isHeAHero;
 
   public Konovalov() {
-    name = "Сифил Лифисов";
-    power = "Изначально был рождён с очень сильной магией огня, из-за чего сошёл с ума";
-    fromUniverse = "Выходец из вселенной, в которой правят драконы и царит средневековье";
-    species = "Человек, рождённый в сливянии дракона и человека";
+      name = "Сифил Лифисов";
+      power = "Изначально был рождён с очень сильной магией огня, из-за чего сошёл с ума";
+      fromUniverse = "Выходец из вселенной, в которой правят драконы и царит средневековье";
+      species = "Человек, рождённый в сливянии дракона и человека";
+      status = "Является выходцем из богатой семьи";
+      age = 150;
+      isHeAHero = false;
     }
+
+    public string GetJobApplication() {
+      string background = $@"Он был рождён во времена драконов,{name}";
+      return background;
+    }
+  }
 }
