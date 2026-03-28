277975393e7c6acf7744cdc8674517cd1cf0c984
Kristina Shcherbakova <kristinagrom876@gmail.com>
2026-03-28 14:54:23 +0700

    change character

--- Group1/Shcherbakova.cs
+++ Group1/Shcherbakova.cs
@@ -2,16 +2,16 @@
 
 namespace ForkKnights.Group1 {
   public class Artur : IKnight {
-    public string Name;
-    public int Experience;
+    public string name;
+    public int experience;
 
     public string Ship { get; set; }
     public string Rank { get; set; }
     public string Treasure { get; set; }
 
     public Artur() {
-      Name = "Артур";
-      Experience = 10;
+      name = "Артур";
+      experience = 10;
       Ship = "Судно, спортивная лодка, парусная яхта, катер, крейсер";
       Rank = "Капитан дальнего плавания";
       Treasure = "Золотые слитки и редкие карты";
@@ -21,8 +21,8 @@ namespace ForkKnights.Group1 {
       return
       "Эй! Капитан ищет команду!\n" +
       "\n" +
-      "Меня зовут " + Name + ".\n" +
-      "Стаж плавания: " + Experience + " лет.\n" +
+      "Меня зовут " + name + ".\n" +
+      "Стаж плавания: " + experience + " лет.\n" +
       "Звание: " + Rank + ".\n" +
       "На чём обычно плаваю: " + Ship + ".\n" +
       "Любимая добыча: " + Treasure + ".\n" +
