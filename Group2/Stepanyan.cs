6a6b148222d7bc1fb0d776271c703ab93a4bd919
v01d <Kevin_Vearez@mail.ru>
2026-04-05 18:19:17 +0700

    The new knight has been created

---
+++ Group2/Stepanyan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkKnights.Group2 {
+  public class Stepanyan : IKnight {
+    public string Name;
+    public string Archetype;
+    public string MainWeapon;
+    public int ArmorIntegrity;
+    public bool IsLookingForParty;
+
+    public Stepanyan() {
+      Name = "Сэр Степанян";
+      Archetype = "Техно-Паладин";
+      MainWeapon = "Квантовый Двуручник";
+      ArmorIntegrity = 100;
+      IsLookingForParty = true;
+    }
+  }
+}
